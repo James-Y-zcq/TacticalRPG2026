1177d0276144557b8ab421305f30d7c79812bc31
David K <davidknight11207@gmail.com>
2026-05-19 17:16:42 -0400

    Audio Manager
    taken from the 2D template

--- Assets/Scripts/Character/Unit.cs
+++ Assets/Scripts/Character/Unit.cs
@@ -7,7 +7,7 @@ public class Unit
 {
     public int level; //the level of the unit applies 
     public UnitBase _base {get; private set;} //this just means it can be accessed but not reassigned by another script (only internally by the constructor). Useful in all sorts of places.
-    
+
     public Unit(UnitBase _base)
     {
         this._base = _base;    
@@ -16,8 +16,8 @@ public class Unit
 
     public void calculateStats()
     {
-        //vitality may have its own calculation for an HP value
-        vitality = level + _base.Vitality;
+        //vitality has its own calculation for an HP value
+        vitality = (level * 3) + (_base.Vitality * 5);
         
         agility = level + _base.Agility;
         strength = level + _base.Strength;
