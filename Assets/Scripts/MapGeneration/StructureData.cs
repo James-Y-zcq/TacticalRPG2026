9f0ba0fb8e5c97eda8ec9909b0cb6ff8264ce9d0
David K <davidknight11207@gmail.com>
2026-06-18 19:09:04 -0400

    Structure Placer and Structure Baker
    Huge progress towards map generation!
    To make a structure, simply build it off of the teststructureprefab, right-click the asset, and bake structure. To test the structure, go to the combatscene and right-click + place structure on the structure placer class.

--- Assets/Scripts/MapGeneration/StructureData.cs
+++ Assets/Scripts/MapGeneration/StructureData.cs
@@ -1,21 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "BattleFieldBuilding/Create new BattlefieldBuilding")]
-public class BattlefieldBuilding : ScriptableObject
+[CreateAssetMenu(menuName = "StructureData/Create new StructureData")]
+public class StructureData : ScriptableObject
 {
     /// <summary>
     /// Tiles that are used in this building. Allows for palette swapping easily to make a building a different culture.
     /// </summary>
-    [SerializeField] List<Sprite> usedTiles;
+    [SerializeField] public List<Sprite> tilePalette;
 
     /// <summary>
     /// List of 2d arrays that represents the indices of the associated tiles on a layer. A development tool will likely be required to accomodate easier construction.
     /// </summary>
-    [SerializeField] List<int[][]> layers;
+    [SerializeField] public List<LayerData> layers;
     
     /// <summary>
     /// Dimensions of the building, in tiles.
     /// </summary>
-    [SerializeField] Vector2 footprint;
+    [SerializeField] public Vector2 footprint;
+}
+
+//Some structs to get around Unity inspector quirks
+
+[System.Serializable]
+public struct ColumnData {
+    public List<int> rows;
+}
+
+[System.Serializable]
+public struct LayerData {
+    public List<ColumnData> columns;
 }
